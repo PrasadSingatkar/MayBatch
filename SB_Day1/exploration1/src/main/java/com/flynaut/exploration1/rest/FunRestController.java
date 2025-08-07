1fc9329dd465a7293e700155b5895fd2ce102ada
Prasad <prasadsingatkar360@gmail.com>
2025-08-07 18:35:26 +0530

    SpringBoot

--- SB_Day1/exploration1/src/main/java/com/flynaut/exploration1/rest/FunRestController.java
+++ SB_Day1/exploration1/src/main/java/com/flynaut/exploration1/rest/FunRestController.java
@@ -3,6 +3,8 @@ package com.flynaut.exploration1.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 public class FunRestController {
 
@@ -13,5 +15,12 @@ public class FunRestController {
     }
 
     //To return today's date
+    //@RequestMapping(value = "/date",method = RequestMethod.GET)
+    @GetMapping("/dateTime")
+    public LocalDate date(){
+        LocalDate localDate = LocalDate.now();
+        return localDate;
+    }
+
     //To return yesterday's date
 }
